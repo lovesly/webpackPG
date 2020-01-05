1f6e6968249865646673804e298115ddbbfbcb38
Zihan Zhang <zzhlovesly@outlook.com>
2020-01-05 22:18:00 +0800

    dynamic import

---
+++ build/2.bundle.js
@@ -0,0 +1,15 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([[2],{
+
+/***/ "./src/sum.js":
+/*!********************!*\
+  !*** ./src/sum.js ***!
+  \********************/
+/*! exports provided: sum */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"sum\", function() { return sum; });\n/* harmony import */ var lodash__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! lodash */ \"./node_modules/lodash/lodash.js\");\n/* harmony import */ var lodash__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(lodash__WEBPACK_IMPORTED_MODULE_0__);\n\nvar sum = lodash__WEBPACK_IMPORTED_MODULE_0___default.a.add;\n\n//# sourceURL=webpack:///./src/sum.js?");
+
+/***/ })
+
+}]);
--- src/index.js
+++ src/index.js
@@ -2,25 +2,22 @@
 // 在家 windows 上没问题。一样的代码，当然也许 node_modules 不一样。
 // 然后，加上 splitChunks 就白屏，根本不加载入口文件？？
 // 如果设置 chunks 为 all 就不自动执行，不明白为什么
-// function component() {
-//     const element = document.createElement('div');
-//     const button = document.createElement('button');
-//     const br = document.createElement('br');
-//     button.innerHTML = 'Click me';
-//     button.onclick = (e) => {
-//         import('./image_viewer');
-//         import('./sum').then(module => {
-//             debugger;
-//             const sum = module.default;
-//         });
-//     }; 
-//     element.appendChild(br);
-//     element.appendChild(button);
-//     return element;
-// }
-// // 为什么非要这样？？
-// document.body.appendChild(component());
-
-import { sum } from './sum';
-import './image_viewer';
-console.log(sum(4, 5));
+function component() {
+    const element = document.createElement('div');
+    const button = document.createElement('button');
+    const br = document.createElement('br');
+    button.innerHTML = 'Click me';
+    button.onclick = (e) => {
+        import('./image_viewer');
+        import('./sum').then(module => {
+            // cjs 模块会到处到 default 上。
+            const { sum } = module;
+            sum(4, 19);
+        });
+    }; 
+    element.appendChild(br);
+    element.appendChild(button);
+    return element;
+}
+// 为什么非要这样？？
+document.body.appendChild(component());
